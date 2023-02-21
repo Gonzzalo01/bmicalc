c738d152791bf476f8fd6e7e2ce331b2341cfbf6
Gonzzalo01 <gonzalo851macias@gmail.com>
2023-02-21 22:08:22 +0100

    Añadido el test de category que comprueba la excepcion y he añadido la explicacion de estos test en el readme

--- README.md
+++ README.md
@@ -3,4 +3,16 @@
 # BMI calculator
 A biomedical calculator for the body mass index (BMI) and other parameters.
 
+Test catValNormalesTest: 
+	-Hacemos un bucle recorriendo una lista donde he incluido unos valores con un cierto orden para que se puedan probar en el método category.
+
+Test catValLimite:
+	-Volvemos a usar un bucle para recorrer otra lista con los valores añadidos, siendo estos valores más especificos para intentar buscar el error, por ejemplo el 24.99
+	
+Test cateLimit2: 
+	-En una lista guardamos unos valores que deberian lanzar una excepcion, por lo que con un bucle y la lista comprobamos que se cumple el lanzamiento de la excepcion esperada.
+	
+
+ 
+
 
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,41 +1,58 @@
 package bmicalc;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Iterator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
-	
-	final double [] ListValNor = {16 ,19.5,27,33}; 
-	final double [] ListValLimit= {17.5,24.99,29.99,30};
-	final String [] CategoriaIMC = {"UNDERWEIGHT","NORMAL","OVERWEIGHT","OBESE"};
+	final double[] ListValLimit2= {0,50,51};
+	final double[] ListValNor = { 16, 19.5, 27, 33 };
+	final double[] ListValLimit = { 17.5, 24.99, 29.99, 30 };
+	final String[] CategoriaIMC = { "UNDERWEIGHT", "NORMAL", "OVERWEIGHT", "OBESE" };
 	BMICalcImpl Ej = new BMICalcImpl();
 
 	@Test
 	public void bmi() {
 		assertEquals(true, true);
 	}
+
 	@Test
-	
+
 	public void catValNormalesTest() {
 		for (int i = 0; i < ListValNor.length; i++) {
 			String c = (CategoriaIMC[i]);
-			
-			assertEquals(c,Ej.category(ListValNor[i]));
-			}
+
+			assertEquals(c, Ej.category(ListValNor[i]));
 		}
-	
-	
+	}
+
 	@Test
 	public void catValLimite() {
 		for (int i = 0; i < ListValNor.length; i++) {
 			String c = (CategoriaIMC[i]);
-			
-			assertEquals(c,Ej.category(ListValLimit[i]));
-			}		
-		
-		
+
+			assertEquals(c, Ej.category(ListValLimit[i]));
+		}
+
+	}
+	
+	@Test 
+	public void cateLimit2() {
+		for (int i = 0; i < ListValLimit2.length; i++) {
+			try {
+				Ej.category(ListValLimit2[i]);
+				fail();
+			}
+			catch(RuntimeException exce){
+				
+			}
+			catch(Exception exce) {
+				fail();
+			}
+		}
 	}
 }
