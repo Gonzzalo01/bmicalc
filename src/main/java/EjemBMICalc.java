fffd400db8e8d669acfd6aa33e2d94baedc7645a
Gonzzalo01 <gonzalo851macias@gmail.com>
2023-02-21 12:48:59 +0100

    Creacion de los metodos y creacion de una clase de ejemplo

---
+++ src/main/java/EjemBMICalc.java
@@ -0,0 +1,12 @@
+import bmicalc.BMICalcImpl;
+
+
+public class EjemBMICalc {
+	public static void main(String[] args) {
+		BMICalcImpl Categ = new BMICalcImpl();
+		
+		System.out.println(Categ.abdominalObesity(90 , 'M')); 
+		
+	}
+
+}
--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -2,16 +2,54 @@ package bmicalc;
 
 public class BMICalcImpl implements BMICalc {
 
+	
+	
 	public double bmi(double mass, double height) {
-		return 0.0;
+		double imc=0;		
+		if(mass <= 0 || height<=0) {
+						
+		}if(mass >500 || height > 2.60) {
+			
+		}else {
+			double h2 = Math.pow(height,2);
+			imc = mass/h2;
+			
+		}
+		
+		return imc;
 	}
 
 	public String category(double bmi) {
-		return "";
+		String Cat = null;
+		if (bmi< 18.5) {
+			Cat= ("OVERWEIGHT");
+		}
+		if (18.5<= bmi && bmi <= 24.9 ) {
+			Cat=("NORMAL");
+		}
+		if (25<=bmi && bmi <=29.9 ) {
+			Cat=("OVERWEIGHT");
+		}
+		if (bmi>30.0 ) {
+			Cat=("OBESE");
+		}
+		
+		return Cat;
+		
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+		
+		boolean obeso = false;
+		
+		if(waistCircumference >= 90 && gender == 'M') {
+			obeso= true;
+		}
+		if(waistCircumference >= 80 && gender == 'F') {
+			obeso=true;
+		}
+		
+		return obeso;
 	}
 
 }
