eb7ba1fd33e667fcd83abb18d6e07fefde757632
Gonzzalo01 <gonzalo851macias@gmail.com>
2023-02-21 21:41:52 +0100

    Test metodo categories hechos, con métodos corregidos

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -2,53 +2,56 @@ package bmicalc;
 
 public class BMICalcImpl implements BMICalc {
 
-	
-	
 	public double bmi(double mass, double height) {
-		double imc=0;		
-		if(mass <= 0 || height<=0) {
-						
-		}if(mass >500 || height > 2.60) {
-			
-		}else {
-			double h2 = Math.pow(height,2);
-			imc = mass/h2;
-			
+		double imc = 0;
+		if (mass <= 0 || height <= 0) {
+
 		}
-		
+		if (mass > 500 || height > 2.60) {
+
+		} else {
+			double h2 = Math.pow(height, 2);
+			imc = mass / h2;
+
+		}
+
 		return imc;
 	}
 
 	public String category(double bmi) {
 		String Cat = null;
-		if (bmi< 18.5) {
-			Cat= ("OVERWEIGHT");
+		if (bmi <= 0 || bmi >= 50) {
+			throw new RuntimeException("The BMI value is not real");
+		} else {
+
+			if (bmi < 18.5) {
+				Cat = ("UNDERWEIGHT");
+			}
+			if (18.5 <= bmi && bmi < 25) {
+				Cat = ("NORMAL");
+			}
+			if (25 <= bmi && bmi < 30) {
+				Cat = ("OVERWEIGHT");
+			}
+			if (bmi >= 30.0) {
+				Cat = ("OBESE");
+			}
 		}
-		if (18.5<= bmi && bmi <= 24.9 ) {
-			Cat=("NORMAL");
-		}
-		if (25<=bmi && bmi <=29.9 ) {
-			Cat=("OVERWEIGHT");
-		}
-		if (bmi>30.0 ) {
-			Cat=("OBESE");
-		}
-		
 		return Cat;
-		
+
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		
+
 		boolean obeso = false;
-		
-		if(waistCircumference >= 90 && gender == 'M') {
-			obeso= true;
+
+		if (waistCircumference >= 90 && gender == 'M') {
+			obeso = true;
 		}
-		if(waistCircumference >= 80 && gender == 'F') {
-			obeso=true;
+		if (waistCircumference >= 80 && gender == 'F') {
+			obeso = true;
 		}
-		
+
 		return obeso;
 	}
 
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -2,14 +2,40 @@ package bmicalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
 	
+	final double [] ListValNor = {16 ,19.5,27,33}; 
+	final double [] ListValLimit= {17.5,24.99,29.99,30};
+	final String [] CategoriaIMC = {"UNDERWEIGHT","NORMAL","OVERWEIGHT","OBESE"};
+	BMICalcImpl Ej = new BMICalcImpl();
+
 	@Test
-	@DisplayName("Example Test Method.")
 	public void bmi() {
 		assertEquals(true, true);
+	}
+	@Test
+	
+	public void catValNormalesTest() {
+		for (int i = 0; i < ListValNor.length; i++) {
+			String c = (CategoriaIMC[i]);
+			
+			assertEquals(c,Ej.category(ListValNor[i]));
+			}
+		}
+	
+	
+	@Test
+	public void catValLimite() {
+		for (int i = 0; i < ListValNor.length; i++) {
+			String c = (CategoriaIMC[i]);
+			
+			assertEquals(c,Ej.category(ListValLimit[i]));
+			}		
+		
+		
 	}
 }
